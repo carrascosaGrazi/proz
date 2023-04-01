1bd5e6042f7743e583d9bec4b2315e75ac37d612
GCarrascosa <45268990+GCarrascosa@users.noreply.github.com>
2023-03-31 21:20:06 -0300

    estudos de laço for e while

--- Atv 02/atv2.py
+++ Atv 02/atv2.py
@@ -1,2 +1,15 @@
-for i in range(20,1, -1):
-    print(i)
+# for i in range(20,0, -1):
+#    print(i)
+
+#for i in range (20): 
+#    i= i +1 
+#    if (i == 13): 
+#        continue
+    
+#    print (i)"" 
+
+i = 20
+while i > 0:
+    if i!=13:
+        print (i)
+    i -= 1
